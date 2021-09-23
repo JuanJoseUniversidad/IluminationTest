c98fe02a247813e04ef4a7b7fc7ccd4e355b8cb1
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-09-23 16:32:55 +0200

    Movement character

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,16 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+      float horizontal = Input.GetAxis("Horizontal");
+      float vertical = Input.GetAxis("Vertical");
+      Vector2 position = transform.position;
+      position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+	  position.y = position.y + 3.0f * vertical * Time.deltaTime;
+      transform.position = position;
     }
 }
