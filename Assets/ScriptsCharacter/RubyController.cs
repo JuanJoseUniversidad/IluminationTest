059f8ea6737cf59b1f450f9310d1e0328cbad8fe
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-07 15:54:15 +0200

    Enviroment damage

--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -7,8 +7,13 @@ public class RubyController : MonoBehaviour
  public float speed = 3.0f;
     public int health { get { return currentHealth; }}
     public int maxHealth = 5;
-    int currentHealth;
+    public float timeInvincible = 2.0f;
+	
+	int currentHealth;
     
+	bool isInvincible;
+    float invincibleTimer;
+	
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -25,6 +30,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+		
+		if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
     
     void FixedUpdate()
@@ -38,6 +50,15 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
