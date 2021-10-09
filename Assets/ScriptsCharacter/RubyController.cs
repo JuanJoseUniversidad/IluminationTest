6a040124477f8de5f84c628385e237bff3f1418c
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-10 00:46:16 +0200

    CreationOfJambi

--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -38,6 +38,15 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+		
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+			if (hit.collider != null)
+			{
+				Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+			}
+		}
     }
     
     void FixedUpdate()
