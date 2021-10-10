84b5e38f1dcb445392047491cfdd3f3352adbd65
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-10-10 02:36:54 +0200

    RaycastDialog

--- Assets/ScriptsCharacter/RubyController.cs
+++ Assets/ScriptsCharacter/RubyController.cs
@@ -65,7 +65,11 @@ public class RubyController : MonoBehaviour
 			RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
 			if (hit.collider != null)
 			{
-				Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+				NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+				if (character != null)
+				{
+					character.DisplayDialog();
+				}  
 			}
 		}
     }
